3abc5a6559bc1cfbd7de204ae4933042314bab5a
Altairyu <ryudaaaai77@gmail.com>
2023-04-30 07:08:44 +0900

    コード改良用にディレクトリをコピー

---
+++ submission_quests/week1_2/blackjack_after_feedback/blackjack.rb
@@ -0,0 +1,9 @@
+require_relative 'blackjack_controller'
+
+game = BlackjackController.new
+game.start
+game.loop_player_turn
+game.loop_dealer_turn
+game.show_final_scores
+game.show_result
+game.finish
---
+++ submission_quests/week1_2/blackjack_after_feedback/deck.rb
@@ -0,0 +1,21 @@
+require_relative 'card'
+
+class Deck
+  def initialize
+    @cards = generate_deck
+  end
+
+  def generate_deck
+    cards = []
+    Card::SUIT.each do |suit|
+      Card::RANKS.each do |rank|
+        cards << Card.new(suit, rank)
+      end
+    end
+    cards.shuffle!
+  end
+
+  def deal
+    @cards.pop
+  end
+end
