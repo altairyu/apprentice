ec9f7f88a9f552124e94623f168f26552e2e2c0d
Altairyu <ryudaaaai77@gmail.com>
2023-04-29 21:12:42 +0900

    リファクタリング用にディレクトリをコピー

---
+++ submission_quests/week1_2/blackjack_after_feedback/hand.rb
@@ -0,0 +1,40 @@
+require_relative 'deck'
+
+class Hand
+  attr_reader :cards
+
+  def initialize
+    @cards = []
+  end
+
+  def add(card)
+    @cards << card
+  end
+
+  def show(participant)
+    puts "#{participant.name}が引いた#{@cards.length}枚目のカードは#{@cards.last.suit}の#{@cards.last.rank}"
+  end
+
+  def score(participant)
+    case @cards.last.rank
+    when 'J', 'Q', 'K'
+      10
+    when 'A'
+      calc_ace_score(participant)
+    else
+      @cards.last.rank.to_i
+    end
+  end
+
+  def calc_ace_score(participant)
+    if 11 + participant.score <= 21
+      11
+    else
+      1
+    end
+  end
+
+  def bust?(score)
+    true if score > 21
+  end
+end
