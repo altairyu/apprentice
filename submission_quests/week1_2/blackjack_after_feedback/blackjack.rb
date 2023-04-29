f9bc720804e6ab40b1ddcac454f106e099f9ad57
Altairyu <ryudaaaai77@gmail.com>
2023-04-29 21:36:53 +0900

    ディレクトリ消去

--- submission_quests/week1_2/blackjack_after_feedback/blackjack.rb
+++
@@ -1,9 +0,0 @@
-require_relative 'blackjack_controller'
-
-game = BlackjackController.new
-game.start
-game.loop_player_turn
-game.loop_dealer_turn
-game.show_final_scores
-game.show_result
-game.finish
